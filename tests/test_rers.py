1e5af552c3405a78fa16f536ef44a4234c0c7f79
tyler <thoff17@hotmail.com>
2023-04-07 17:56:39 -0400

    feat: Added Ranking class and associated functions. build: Added PrettyTable dependency

--- tests/test_rers.py
+++ tests/test_rers.py
@@ -5,6 +5,17 @@ def test_version():
     assert __version__ == "0.1.0"
 
 
-def test_team_create():
+def test_team_class():
     team = rers.Team(get_cfbd.get_random_team(year=2023))
     assert team is not None
+
+
+def test_ranking_class():
+    ranking = rers.Ranking()
+    ranking.initialize_rankings(method="random")
+
+
+def test_print_rankings():
+    ranking = rers.Ranking()
+    ranking.initialize_rankings(method="random")
+    ranking.print_rankings()
